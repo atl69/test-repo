4b2b7e48300690dd7382f5c171ad8137a4ecaf79
atl69 <andrzej.le@gmail.com>
2020-10-07 16:15:41 +0200

    my first file in repo

---
+++ kasjerka.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+
+print("Jak masz na imię?")
+imie = input()
+print(f"Cześć {imie}, miło cię poznać. \nIle masz lat? ")
+wiek = float(input())
+print("Z jakiego peronu odjeżdża pociąg do Hogwartu?")
+peron = input()
+print(f"W wieku {round(wiek)} lat już chyba nie ma sensu udawać się na peron {peron}.")
